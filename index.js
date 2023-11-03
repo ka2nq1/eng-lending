bbb4ffdff4f9090358a679062d573a9e0e71d0b6
v.pastukh <v.pastukh@foodtech.team>
2023-11-03 19:30:56 +0200

    new blocks

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const menu = document.querySelector('.burger-menu');
 const button = menu.querySelector('.burger-menu_button, .burger-menu_lines');
 const links = menu.querySelectorAll('.burger-menu_link, .close-menu');
 const overlay = menu.querySelector('.burger-menu_overlay');
+const certificates = document.querySelector('.certificates');
+const showMoreBtn = certificates.querySelectorAll('.show-more, .hide');
+
 button.addEventListener('click', (e) => {
     e.preventDefault();
     toggleMenu();
@@ -18,4 +21,14 @@ function toggleMenu() {
     } else {
         document.body.style.overflow = 'visible';
     }
+}
+
+showMoreBtn.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    toggleCertificates();
+})});
+
+function toggleCertificates() {
+    certificates.classList.toggle('open');
 }
